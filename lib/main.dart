ef10a54bae4ff7e4aa9eb62a1c2b488679ba30e7
Daniel H <daguiheso@gmail.com>
2019-11-11 16:28:01 -0500

    Create GradientBack widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'review_list.dart';
 import 'description_place.dart';
+import 'gradient_back.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  String descriptionDummy = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,15 +23,20 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Mi primer App'),
-        ),
-        // body: DescriptionPlace('Bahamas', 4, 'Lorem ipsum panim fer reds')
-        body: ReviewList()
-      ) ,
+        body: Stack(
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+                DescriptionPlace('Bahamas', 4, descriptionDummy),
+                ReviewList()
+              ],
+            ),
+            GradientBack(),
+          ],
+        )),
     );
   }
 }
