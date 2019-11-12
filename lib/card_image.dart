c10e3b4748b2ff3cd16040ee832a094bc41caacc
Daniel H <daguiheso@gmail.com>
2019-11-11 22:52:01 -0500

    Create  FavButton  StatefulWidget

--- lib/card_image.dart
+++ lib/card_image.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'fav_button.dart';
 
 class CardImage extends StatelessWidget {
   String pathImage;
@@ -30,6 +31,13 @@ class CardImage extends StatelessWidget {
         ]
       ),
     );
-    return card;
+
+    return Stack(
+      alignment: Alignment(0.9, 1.1),
+      children: <Widget>[
+        card,
+        FavButton()
+      ],
+    );
   }
 }
