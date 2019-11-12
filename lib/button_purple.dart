777c12d9375a6f30b0ad9226b2be7db1d9d3f25f
Daniel H <daguiheso@gmail.com>
2019-11-11 20:37:12 -0500

    add OnTap to ButtonPurple widget

--- lib/button_purple.dart
+++ lib/button_purple.dart
@@ -8,32 +8,35 @@ class ButtonPurple extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: () {
+        Scaffold.of(context).showSnackBar(
+          SnackBar(content: Text('Navegando'))
+        );
+      },
       child: Container(
-        margin: EdgeInsets.only(top: 20, left: 20, right: 20),
-        height: 50,
-        width: 180,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          gradient: LinearGradient(
-            colors: [Color(0xFF4268D3), Color(0xFF584CD1)],
-            begin: FractionalOffset(0.2, 0),
-            end: FractionalOffset(1, 0.6),
-            stops: [0, 0.6],
-            tileMode: TileMode.clamp
+          margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+          height: 50,
+          width: 180,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            gradient: LinearGradient(
+                colors: [Color(0xFF4268D3), Color(0xFF584CD1)],
+                begin: FractionalOffset(0.2, 0),
+                end: FractionalOffset(1, 0.6),
+                stops: [0, 0.6],
+                tileMode: TileMode.clamp),
           ),
-        ),
-        child: Center(
-          child: Text(
-            text,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontFamily: 'Lato',
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontFamily: 'Lato',
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
-          ),
-        )
-      ),
+          )),
     );
   }
 }
