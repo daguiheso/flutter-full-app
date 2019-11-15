7df7d87b114b7b2cc29c769599e621c7f8dddf9f
Daniel H <daguiheso@gmail.com>
2019-11-14 20:07:17 -0500

    Create ProfileTrips widget

--- lib/profile/profile_trips.dart
+++ lib/profile/profile_trips.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_flutter/profile/header_profile.dart';
+import 'package:hello_world_flutter/profile/profile_bar_actions.dart';
+import 'package:hello_world_flutter/shared/gradient_back.dart';
 
 class ProfileTrips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.orangeAccent);
-  }
 
+    return Stack(
+      children: <Widget>[
+        GradientBack('Profile', 400, -0.9, -0.72),
+        Container(
+          margin: EdgeInsets.only(left: 20, top: 100, right: 20),
+          child: Column(
+            children: <Widget>[
+              HeaderProfile(),
+              ProfileBarActions(),
+            ],
+          ),
+        )
+      ],
+    );
+  }
 }
