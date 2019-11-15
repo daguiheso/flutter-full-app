ea890bbaef2746c39dd1acfdfbf56e9db8f43721
Daniel H <daguiheso@gmail.com>
2019-11-14 20:07:04 -0500

    Create ProfileBarAction widget

---
+++ lib/profile/profile_bar_actions.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ProfileBarActions extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ButtonBar(
+        mainAxisSize: MainAxisSize.min, // this will take space as minimum as posible(to center)
+        children: <Widget>[
+          FloatingActionButton(
+            child: Icon(Icons.bookmark_border),
+            mini: true,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.deepPurpleAccent,
+            onPressed: null,
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.assignment),
+            mini: true,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.deepPurpleAccent,
+            onPressed: null,
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.add, size: 40,),
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.deepPurpleAccent,
+            onPressed: null,
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.mail_outline),
+            mini: true,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.deepPurpleAccent,
+            onPressed: null,
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.person),
+            mini: true,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.deepPurpleAccent,
+            onPressed: null,
+          ),
+        ],
+      ),
+    );;
+  }
+
+}
