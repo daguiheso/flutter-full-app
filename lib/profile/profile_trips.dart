aa40a015036c58f4c47cbff3ad98f8a13ff48044
Daniel H <daguiheso@gmail.com>
2019-11-16 14:41:55 -0500

    Refactor ProfileTrips widget

--- lib/profile/profile_trips.dart
+++ lib/profile/profile_trips.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_flutter/profile/card_info_list.dart';
 import 'package:hello_world_flutter/profile/header_profile.dart';
-import 'package:hello_world_flutter/profile/profile_bar_actions.dart';
 import 'package:hello_world_flutter/shared/gradient_back.dart';
 
 class ProfileTrips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Stack(
       children: <Widget>[
-        GradientBack('Profile', 400, -0.9, -0.72),
-        Container(
-          margin: EdgeInsets.only(left: 20, top: 100, right: 20),
-          child: Column(
-            children: <Widget>[
-              HeaderProfile(),
-              ProfileBarActions(),
-            ],
-          ),
+        GradientBack('', 400, -0.9, -0.72),
+        Stack(
+          children: <Widget>[
+            ListView(children: <Widget>[CardInfoList()]),
+            HeaderProfile()
+          ],
         )
       ],
     );
