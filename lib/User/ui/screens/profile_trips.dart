78bf2cfb6c589e4be5875433da7feee220fe8e4a
Daniel H <daguiheso@gmail.com>
2019-11-20 09:47:48 -0500

    Call  new widgets in ProfileTrips widget

--- lib/User/ui/screens/profile_trips.dart
+++ lib/User/ui/screens/profile_trips.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world_flutter/User/ui/screens/header_profile.dart';
-import 'package:hello_world_flutter/User/ui/widgets/card_info_list.dart';
-import 'package:hello_world_flutter/widgets/gradient_back.dart';
+import 'package:hello_world_flutter/User/ui/screens/profile_header.dart';
+import 'package:hello_world_flutter/User/ui/widgets/profile_background.dart';
+import 'package:hello_world_flutter/User/ui/widgets/profile_places_list.dart';
 
 class ProfileTrips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        GradientBack('', 400, -0.9, -0.72),
-        Stack(
-          children: <Widget>[
-            ListView(children: <Widget>[CardInfoList()]),
-            HeaderProfile()
-          ],
-        )
+        ProfileBackground(),
+        ListView(children: <Widget>[
+          ProfileHeader(),
+          ProfilePlacesList()
+        ]),
       ],
     );
   }
