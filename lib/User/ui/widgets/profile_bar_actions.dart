4660c79ded522fcfa60064bf732e81011a43ad77
Daniel H <daguiheso@gmail.com>
2019-11-20 09:44:28 -0500

    Refactor ProbileBarActions

--- lib/User/ui/widgets/profile_bar_actions.dart
+++ lib/User/ui/widgets/profile_bar_actions.dart
@@ -1,46 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_flutter/User/ui/widgets/circle_button.dart';
 
 class ProfileBarActions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ButtonBar(
-        mainAxisSize: MainAxisSize.min, // this will take space as minimum as posible(to center)
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 0.0,
+        vertical: 10.0
+      ),
+      child: Row(
         children: <Widget>[
-          FloatingActionButton(
-            child: Icon(Icons.bookmark_border),
-            mini: true,
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.deepPurpleAccent,
-            onPressed: null,
-          ),
-          FloatingActionButton(
-            child: Icon(Icons.assignment),
-            mini: true,
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.deepPurpleAccent,
-            onPressed: null,
-          ),
-          FloatingActionButton(
-            child: Icon(Icons.add, size: 40,),
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.deepPurpleAccent,
-            onPressed: null,
-          ),
-          FloatingActionButton(
-            child: Icon(Icons.mail_outline),
-            mini: true,
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.deepPurpleAccent,
-            onPressed: null,
-          ),
-          FloatingActionButton(
-            child: Icon(Icons.person),
-            mini: true,
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.deepPurpleAccent,
-            onPressed: null,
-          ),
+          CircleButton(true, Icons.bookmark_border, 20.0, Color.fromRGBO(255, 255, 255, 1)),
+          CircleButton(true, Icons.assignment, 20.0, Color.fromRGBO(255, 255, 255, 0.6)),
+          CircleButton(false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1)),
+          CircleButton(true, Icons.mail_outline, 20.0, Color.fromRGBO(255, 255, 255, 0.6)),
+          CircleButton(true, Icons.person, 20.0, Color.fromRGBO(255, 255, 255, 0.6))
         ],
       ),
     );
