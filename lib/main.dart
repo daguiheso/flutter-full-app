5c9aa1ddf41ee4d8e30497468916afcf510fa534
Daniel H <danielhernandez16@hotmail.com>
2022-03-09 19:13:49 -0500

    feat(bloc-provider): add basic BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:button_animate/User/bloc/bloc_user.dart';
 import 'package:button_animate/navigation_trips.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
@@ -15,23 +17,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo lol',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page KIKI'),
-      home: const NavigationTrips(),
-    );
+    return BlocProvider(
+        child: const MaterialApp(
+          title: 'Flutter Demo lol',
+          home: NavigationTrips(),
+        ),
+        bloc: UserBloc());
   }
 }
 
