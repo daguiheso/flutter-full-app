fe2d8b982849a45fd0fd46350ff3bfce7735a8aa
Daniel H <danielhernandez16@hotmail.com>
2022-03-10 19:59:05 -0500

    chore(firebase-core): install and initialize

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:button_animate/navigation_trips.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent, // transparent status bar
   ));
